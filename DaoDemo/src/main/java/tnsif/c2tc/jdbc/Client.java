e2a41f379602f0d9590eeac18aa2b3bf48e0b589
AFZAL <AFZAL@User-PC>
2022-03-04 14:51:34 +0530

    JDBC Revision using DAO Layer.

--- DaoDemo/src/main/java/tnsif/c2tc/jdbc/Client.java
+++ DaoDemo/src/main/java/tnsif/c2tc/jdbc/Client.java
@@ -1,9 +1,11 @@
 package tnsif.c2tc.jdbc;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Client {
 
 	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
 		//Student s = new Student();
 		//s.setUid(606);
 		//s.setName("Frank");
@@ -20,6 +22,17 @@ public class Client {
 			System.out.println("Age of the student is : "+rs.getInt(3));
 			System.out.println("-------------------------------------------");
 		}
+		/*
+		String name = sc.nextLine();
+		int uid = sc.nextInt();
+		sa.updateStudent(uid,name);
+		System.out.println("The Student details are Updated");
+		*/
+		
+		int uid = sc.nextInt();
+		sa.deleteStudent(uid);
+		System.out.println("The Student details are Updated");
+		
 	}
 
 }
--- DaoDemo/src/main/java/tnsif/c2tc/jdbc/StudentAccess.java
+++ DaoDemo/src/main/java/tnsif/c2tc/jdbc/StudentAccess.java
@@ -4,13 +4,19 @@ import java.sql.*;
 
 public class StudentAccess {
 
-	public void addStudent(Student s) throws Exception {
+	public Connection setup() throws Exception
+	{
 		String url = "jdbc:mysql://localhost:3306/jdbcdemo";
 		String user = "root";
 		String pass = "afzal123";
-		String insert_query = "INSERT INTO `jdbcdemo`.`details` VALUES (?,?,?);";
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection(url,user,pass);
+		return con;
+		
+	}
+	public void addStudent(Student s) throws Exception {
+		Connection con = setup();
+		String insert_query = "INSERT INTO `jdbcdemo`.`details` VALUES (?,?,?);";
 		PreparedStatement st = con.prepareStatement(insert_query);
 		st.setInt(1,s.getUid());
 		st.setString(2,s.getName());
@@ -23,16 +29,33 @@ public class StudentAccess {
 	}
 
 	public ResultSet getStudent() throws Exception {
-		String url = "jdbc:mysql://localhost:3306/jdbcdemo";
-		String user = "root";
-		String pass = "afzal123";
+		Connection con = setup();
 		String query = "SELECT * FROM `details`;";
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection(url,user,pass);
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		
 		return rs;
 	}
 
+	public void updateStudent(int uid, String name) throws Exception {
+		Connection con = setup();
+		String query = "UPDATE `jdbcdemo`.`details` SET `name` = ? WHERE(uid = ?);";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(2, uid);
+		st.setString(1, name);
+		st.executeUpdate();
+		st.close();
+		con.close();
+	}
+	
+	
+	public void deleteStudent(int uid) throws Exception {
+		Connection con = setup();
+		String query = "DELETE FROM `jdbcdemo`.`details` WHERE (uid = ?);";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, uid);
+		st.executeUpdate();
+		st.close();
+		con.close();
+	}
 }
